d60daa7107e7440d6588b7362f46b0c59332dc38
Ferdi Rahmad Rizaldi <official.ferdirizaldi@gmail.com>
2025-06-02 15:56:56 +0700

    first commit no AI implementation

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { AIForm } from "@/components/form";
+
+export default function Home() {
+  return (
+    <main className="max-w-screen-lg m-auto">
+        <AIForm />
+      </main>
+  );
+}
